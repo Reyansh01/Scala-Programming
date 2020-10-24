9b5e79d0063a005ea8561664269c07d52dd14667
Reyansh Mishra <51244218+Reyansh01@users.noreply.github.com>
2020-10-24 18:45:11 +0530

    Decimal upto 2 place

---
+++ Decimal.scala
@@ -0,0 +1,7 @@
+var price = 5.50   // --> price: Double = 5.5
+
+var total = 10*5.5  // ---> total: Double = 55.0
+
+ptrintln(f"Total cost of 10 books = $total%1.2f")
+// output ---> Total cost of 10 books = 55.00
+// upto 2 decimal places.
