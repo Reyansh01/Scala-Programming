de8a412d9ac60e6215f8d0389176760f881db83a
Reyansh Mishra <51244218+Reyansh01@users.noreply.github.com>
2020-10-24 18:48:01 +0530

    Positive or Negative

---
+++ PositiveNegative.scala
@@ -0,0 +1,13 @@
+def check(number : Int) = {
+	if(number > 0)
+	{
+		println("Number is POSITIVE")
+	}
+	else
+	{
+		println("Number is negative")
+	}
+}
+
+check(22)  // ---> Number is POSITIVE.
+check(-3)  // ---> Number is NEGATIVE.
