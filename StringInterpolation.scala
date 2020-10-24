42a7d54d5d6828012006ee5b62277434ae4f85ca
Reyansh Mishra <51244218+Reyansh01@users.noreply.github.com>
2020-10-24 18:51:26 +0530

    String Interpolation

---
+++ StringInterpolation.scala
@@ -0,0 +1,10 @@
+var name = "Reyansh"
+var marks = "90"
+
+// In all cases output will be "Reyansh got 90 marks."
+
+println(name + " got " + marks + " marks.")
+
+println(s"$name got $marks marks.")
+
+println(f"$name%s got $marks%d marks.")
