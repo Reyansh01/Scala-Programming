3f626170b7c2137d9e76b03a871553072d684e59
Reyansh Mishra <51244218+Reyansh01@users.noreply.github.com>
2020-10-24 18:43:01 +0530

    Array Basics

---
+++ Array.scala
@@ -0,0 +1,6 @@
+//Array in scala
+
+var s1 = new Array[String](4) // declaration.
+s1(0) = "apple", s1(1) = "banana", s1(2) = "orange", s1(3) = "Guava"
+
+var s2 = Array("apple","banana","orange") // declaration with definition.
