207b391916c841f5db42fd3c441e049d3130e59d
Reyansh Mishra <51244218+Reyansh01@users.noreply.github.com>
2020-10-24 18:44:35 +0530

    Basics of Class

---
+++ Class.scala
@@ -0,0 +1,14 @@
+// Class in scala
+
+class Student(var name : String, marks : Int)  // constructor of the class.
+{
+	println(name)
+	println(marks)
+}
+
+var obj = new Student("Reyansh",98)  // creation of object
+
+/*
+Reyansh
+95
+*/
