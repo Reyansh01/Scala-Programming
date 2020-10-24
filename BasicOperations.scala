6eb2ad666c229091b1f25acc3da745229ec19dae
Reyansh Mishra <51244218+Reyansh01@users.noreply.github.com>
2020-10-24 18:43:43 +0530

    Mathematical operations

---
+++ BasicOperations.scala
@@ -0,0 +1,13 @@
+var num1 = 10
+
+var num2 = 20
+
+//Basic mathematical operations.
+
+println(num1 + num2)   // ---> 30
+
+println(num1 - num2)   // ---> -10
+
+println(num1 / num2)   // ---> 0
+
+println(num1 % num2)   // ---> 10
