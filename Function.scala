d1dda3ca711b03a0cc1d0ebab94c46fcf5a996ad
Reyansh Mishra <51244218+Reyansh01@users.noreply.github.com>
2020-10-24 18:46:41 +0530

    Basic Function

---
+++ Function.scala
@@ -0,0 +1,9 @@
+// How to write a basic function in scala
+
+def add(a : Int = 10, b : Int = 20) : Int = a + b;
+
+println(add())   // ---> 30
+
+println(add(30))   // ---> 50
+
+println(add(30,40))  // ---> 70
